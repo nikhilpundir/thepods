f0ceb37654ca1b6de6494d758ec62ca51bcdfebd
Nikhil Pundir <nroyalrana@gmail.com>
2023-12-27 12:51:46 +0530

    server initial setup

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import cors from "cors";
+const port = process.env.PORT || 5000;
+
+
+const app = express();
+
+// app.use(cors({
+//     origin:,
+//     methods:["POST","GET"],
+//     credentials:true
+// }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
