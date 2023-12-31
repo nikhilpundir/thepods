ca5efc524872b569f8adda7c7e2ed19319206bb3
Nikhil Pundir <nroyalrana@gmail.com>
2024-01-01 02:34:28 +0530

    routing initial

--- client/src/slices/usersApiSlice.js
+++ client/src/slices/usersApiSlice.js
@@ -1,5 +1,6 @@
 import { apiSlice } from './apiSlice';
-const USERS_URL = '/api/users';
+const BASE_URL = 'https://thepods-server.vercel.app';
+const USERS_URL = `${BASE_URL}/api/users`;
 
 export const userApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
