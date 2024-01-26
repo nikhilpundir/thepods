47da2ae5d6c6d3fb5558d940c51d23648e778f8f
Nikhil Pundir <nroyalrana@gmail.com>
2024-01-26 17:28:19 +0530

    protected debug 1

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -6,32 +6,24 @@ const protect = asyncHandler(async (req, res, next) => {
   let token;
 
   token = req.cookies.jwt;
-  console.log('Token:', token);
-  
-  // if (token) {
   try {
     if (token) {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       const user = await User.findById(decoded.userId).select('-password');
-
-      // Check if the user is marked as deleted
       if (user && user?.isDeleted) {
-        res.status(401);
         throw new Error('Not authorized, user is deleted');
       }
-
+      
       req.user = user;
-
       next();
+
     } else {
-      res.status(401);
       throw new Error('Not authorized, no token');
     }
   } catch (error) {
     console.error(error);
     res.status(401);
-    throw new Error('Not authorized, token failed');
   }
   
 });
