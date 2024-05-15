b3ab70c04518bdd8a42d306ddbc9e20116b3dab9
Nikhil Pundir <nroyalrana@gmail.com>
2024-05-15 19:17:19 +0530

    cancel booking improve 2

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -110,9 +110,9 @@ const deleteBooking=asyncHandler(async(req,res)=>{
       return res.status(404).json({ message: 'booking not found' });
     }
 
-    res.status(200).json({ message: 'Your booking is cancelled successfully' });
+    res.status(200).json({ message: 'Your booking is cancelled successfully',success:true });
   } catch (error) {
-    res.status(500).json({ message: 'Server Error', error: error.message });
+    res.status(500).json({ message: 'Server Error',success:false, error: error.message });
   }
 });
 export { 
